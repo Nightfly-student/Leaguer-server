58e67ca184fa9def07b7910e65f48d77e322fd92
Nightfly-student <70700436+Nightfly-student@users.noreply.github.com>
2022-03-15 11:06:29 +0100

    PORT env <3

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   app.get(/.*/, (req, res) => res.sendFile("/public/index.html"));
 }
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
